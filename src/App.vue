f22923b42b421cbe48dfa9ae2869b362880fe290
FlabyKare <beslansoblirov074@gmail.com>
2023-05-01 11:24:07 +0300

    Minimized the reading JS scripts

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,24 @@ onMounted(() => {
          document.body.classList.remove("preloader");
       }, 250);
    });
+
+   //! Плавная прогрузка Элементов
+   function onEntry(entries, observer) {
+      entries.forEach((entry) => {
+         if (entry.isIntersecting) {
+            entry.target.classList.add("element-show");
+            observer.unobserve(entry.target);
+         }
+      });
+   }
+
+   let observer = new IntersectionObserver(onEntry, {
+      threshold: 0.25,
+      rootMargin: "-5px 0px",
+   });
+   document.querySelectorAll(".element-animation").forEach((elm) => {
+      observer.observe(elm);
+   });
 });
 </script>
 
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,4 +5,22 @@ export default function allScripts() {
          document.body.classList.remove("preloader");
       }, 250);
    });
+
+   //! Плавная прогрузка Элементов
+   function onEntry(entries, observer) {
+      entries.forEach((entry) => {
+         if (entry.isIntersecting) {
+            entry.target.classList.add("element-show");
+            observer.unobserve(entry.target);
+         }
+      });
+   }
+
+   let observer = new IntersectionObserver(onEntry, {
+      threshold: 0.25,
+      rootMargin: "-5px 0px",
+   });
+   document.querySelectorAll(".element-animation").forEach((elm) => {
+      observer.observe(elm);
+   });
 }
